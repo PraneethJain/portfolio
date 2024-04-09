bfc20ef48476310fc5f75de3c8765f50af800152
PraneethJain <praneethjain005@gmail.com>
2024-04-09 14:13:27 +0530

    feat: variadic div

--- html.asm
+++ html.asm
@@ -8,7 +8,7 @@ db '  <head>', 10
 db '    <meta charset="UTF-8" />', 10
 db '    <title>',title,'</title>', 10
 db '  </head>', 10
-db '  <body>', 10
+db '  <body style="padding: 0; margin: 0;">', 10
 }
 
 macro html_end
@@ -17,7 +17,26 @@ db '  </body>', 10
 db '</html>', 10
 }
 
+macro ! text
+{
+db text, 10
+}
+
 macro h i, text
 {
 db '<h',i+'0','>',text,'</h',i+'0','>', 10
+}
+
+macro link href, component
+{
+db '<a href="',href,'">'
+component
+db '</a>', 10
+}
+
+macro div style, [components]
+{
+common db '<div style="',style,'">', 10
+forward components
+common db '</div>', 10
 }
--- responses.asm
+++ responses.asm
@@ -7,10 +7,8 @@ index_page_response  db "HTTP/1.1 200 OK", CRLF
                      db 0
 
 index_page           db 10
-                     html_start "Nice Title"
-                     h 1, "This is an h1"
-                     h 2, "This is an h2"
-                     h 3, "This is an h3"
-                     h 4, "This is an h4"
-                     h 5, "This is an h5"
+                     html_start "Praneeth Jain"
+                     div "display: flex; width: 100%; align-items: center; justify-content: center;", \
+                        ! "HELLO", \
+                        ! "BYE"
                      html_end
